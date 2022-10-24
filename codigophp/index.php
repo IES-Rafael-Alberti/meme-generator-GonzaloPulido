149200ac31631c6576fdf7e51169f4b552c253a3
GonzaloPulido <gpulido.alumno@iesrafaelalberti.es>
2022-10-24 14:22:10 +0200

    primer commit

--- codigophp/index.php
+++ codigophp/index.php
@@ -4,10 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página principal</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>Meme Generator</title>
 </head>
 <body>
-    <a href="phpinfo.php">phpinfo()</a>
-    <a href="xdebug_info.php">xdebug_info()</a>
+    <header> 
+        <a ><img src="img/xerez.png" id="logo" alt="logo-xerez"></a>
+        <div>
+            <?php 
+            
+            ?>
+            <a href=""><i class="fa-solid fa-power-off"></i></a>
+        </div>
+    </header>
 </body>
 </html>
