d61c44ec0fd8e99de49bf739c16d73f7780f74d1
GonzaloPulido <gpulido.alumno@iesrafaelalberti.es>
2022-10-25 19:02:22 +0200

    muchas cosas que me ha explicado david

--- codigophp/index.php
+++ codigophp/index.php
@@ -9,20 +9,52 @@ require("logintest.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/styles.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Meme Generator</title>
 </head>
 <body>
     <header> 
-        <a href="index.php"><img src="img/xerez.png" id="logo" alt="logo-xerez"></a>
+        <a href="index.php"><img alt="Cambiar imagen al pasar el raton" onmouseout="this.src='img/xerez.png';" onmouseover="this.src='img/cadi.png';" src="img/xerez.png" id="logo"/></a>
         <h1 id="title">MEME GENERATOR</h1>
         <nav>
             <?php 
-            print("a");
+            print($_SESSION['nombre']);
             ?>
-            <a href=""><i class="fa-sharp fa-solid fa-power-off"></i></a>
+            <a href="logout.php"><i class="fa-sharp fa-solid fa-power-off"></i></a>
         </nav>
     </header>
+    <section>
+        <article>
+            <?php
+            //url for meme list
+            $url = 'https://api.imgflip.com/get_memes';
+            
+            //open connection
+            $ch = curl_init();
+            
+            //set the url
+            curl_setopt($ch,CURLOPT_URL, $url);
+            
+            //So that curl_exec returns the contents of the cURL; rather than echoing it
+            curl_setopt($ch,CURLOPT_RETURNTRANSFER, true); 
+            
+            //receive url content 
+            $result = curl_exec($ch);
+            
+            //decode content (assoc array)
+            $data = json_decode($result, true);
+            
+            //if success shows images
+            if($data["success"]) {
+                //iterates over memes array
+                foreach($data["data"]["memes"] as $meme) {
+                    //show meme image
+                    echo "<a><img width='150px' padding='5px' src='" . $meme["url"] . "'></a>";
+                }
+            }
+            ?>
+        </article>
+    </section>
 </body>
 </html>
 
